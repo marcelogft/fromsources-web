5cf91fd11fb908a9b3fd695406dc3bfaada7ce7e
Colomer Cornejo <MOCR@gft.com>
2017-11-12 12:33:02 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'from-sources-web',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Fromsources Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#8cb5f2' },
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    '~/assets/style/app.styl'
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: [
+      '~/plugins/vuetify.js'
+    ],
+    extractCSS: true,
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-parallax src="/images/codecode.jpg">
+    <v-layout column align-center justify-center>
+      <h1 class="white--text">Fromsources</h1>
+      <h4 class="white--text">{ coding: true }</h4>
+    </v-layout>
+  </v-parallax>
+</template>
+<style>
+ 
+.parallax__image-container {
+  opacity: 0.1;
+}
+h1 {
+  text-shadow: 4px 4px #000000;
+}
+h4 {
+  text-shadow: 4px 4px #000000;
+}
+</style>
