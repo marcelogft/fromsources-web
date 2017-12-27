8f257cfc7c49e7cb923b01f2b3297030692762ed
Colomer Cornejo <MOCR@gft.com>
2017-12-27 22:43:06 +0100

    cards

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer 
-      persistent
-      :mini-variant="!miniVariant"
-      :clipped="clipped"
+  <v-app id="fromsources" dark>
+    <v-navigation-drawer
+      clipped
+      fixed
       v-model="drawer"
       app
     >
@@ -24,79 +23,36 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer" ></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>FromSources</v-toolbar-title>
     </v-toolbar>
-    <main>
-      <v-content>
-        <v-container>
-          <nuxt />
-        </v-container>
-      </v-content>
-    </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-       <span>Fromsources &copy; 2017</span>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+            <nuxt />
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>Fromsources &copy; 2017</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'home', title: 'Welcome', to: '/' },
-          { icon: 'info', title: 'About', to: '/about' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Fromsources'
-      }
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'home', title: 'Home', to: '/' },
+        { icon: 'info', title: 'About', to: '/about' }
+      ]
+    }),
+    props: {
+      source: String
     }
   }
 </script>
+
+ 
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,34 @@
-<template> 
- <v-container grid-list-xl>
-    <v-layout v-bind="binding">
-      <v-flex>
+<template>  
+<v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
          <v-parallax src="images/codecode.jpg">
            <v-layout column align-center justify-center>
-             <h1 class="white--text">Fromsources</h1>
-             <h4 class="white--text">{ coding: true }</h4>
+             <h1 class="white--text">FromSources</h1>
+             <h4 class="white--text">{ "coding": true }</h4>
            </v-layout>
          </v-parallax>
       </v-flex> 
-    </v-layout>
-  </v-container> 
+      <v-flex xs2 v-for="item in data" :key="item.id">
+        <CardInfo  :info="item"></CardInfo>
+      </v-flex>
+    </v-layout> 
+</v-container>
 </template>
+<script>
+import data from '../static/data/data.json'
+import CardInfo from '../components/cardinfo/cardinfo'
+export default {
+  components: {
+    CardInfo
+  },
+  data () {
+    return {
+      data
+    }
+  }
+}
+</script>
  
 <style>
 .parallax__image-container {
@@ -19,8 +36,11 @@
 }
 h1 {
   text-shadow: 4px 4px #000000;
+  font-size: 4.5em;
+  font-style: italic;
 }
 h4 {
   text-shadow: 4px 4px #000000;
+  font-size: 1.5em;
 }
-</style>
+</style> 
