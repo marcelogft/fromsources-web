06f0f02054fcc1ba3f6d49a6598e3599fad61d35
marcelogft <marcelo.colomer@gft.com>
2018-12-28 09:56:38 +0000

    remove

--- pages/framework.vue
+++
@@ -1,9 +0,0 @@
-<template> 
-<v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs12>
-          hello
-      </v-flex>
-    </v-layout>
-</v-container>
-</template>
--- plugins/vuetify.js
+++
@@ -1,7 +0,0 @@
-import Vuetify from 'vuetify'
-import Vue from 'vue'
-
-import 'material-design-icons/iconfont/material-icons.css'
-import 'typeface-roboto/index.css'
-
-Vue.use(Vuetify)
