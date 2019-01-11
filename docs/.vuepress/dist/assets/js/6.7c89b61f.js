20577f2cee3afb178b6855b95760865661e0b48c
marcelogft <marcelo.colomer@gft.com>
2019-01-11 23:09:42 +0100

    remove dist

--- docs/.vuepress/dist/assets/js/6.7c89b61f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{176:function(t,e,n){"use strict";n.r(e);var s=n(0),i=Object(s.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"content"},[e("About")],1)},[],!1,null,null,null);i.options.__file="Readme.md";e.default=i.exports}}]);
